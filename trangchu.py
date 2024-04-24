864a7c34068385f6bfd3a2a66a4d3cc24ed73170
Quangdung090 <ttramquangdung@gmail.com>
2024-04-24 14:23:23 +0700

    clear both text and image button

--- trangchu.py
+++ trangchu.py
@@ -76,6 +76,22 @@ class TrangChu(CTkFrame):
             command=self.clearTextBtn_command
         )
         clearTextBtn.place(x=900,y=90)
+        
+        clearAllBtn = CTkButton(
+            master=self,
+            width=150,
+            height=45,
+            fg_color="#f0f7ee",
+            hover_color="#8c8b8b",
+            font=("Arial Bold", 12),
+            text="Clear all",
+            text_color="#000000",
+            border_color="#cccccc",
+            border_width=1,
+            corner_radius=0,
+            command=self.clearAllBtn_command
+        )
+        clearAllBtn.place(x=450,y=90)
 
         global imageLabel
         imageLabel = CTkLabel(
@@ -396,6 +412,18 @@ class TrangChu(CTkFrame):
         # Ẩn hết nút
         self.resetDisplay()
 
+    def clearAllBtn_command(self):
+        print("Clear both image & text")
+        output = resultText.get(1.0,END)
+        if(output != "Your text goes here\n"):
+            self.ChangeText("")
+        self.image_selected = False
+        self.image_path = None
+        img = Image.open("img/image.png")
+        nullImg = CTkImage(light_image=img)
+        imageLabel.configure(image = nullImg)
+        imageLabel.configure(text="Click on 'Choose file to upload' to put image here")
+        self.resetDisplay()
 
     def catAnhBtn_command(self):
         print("Cắt ảnh")
